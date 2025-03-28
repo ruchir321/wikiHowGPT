e254617a280a451e3d5ead55eb56a36e8b0aa1c6
ruchir321 <ruchir.attri99@gmail.com>
2025-03-28 14:42:55 -0400

    semantic cach hit successful. TODO: fix cache update when missed.

--- code/chain.py
+++ code/chain.py
@@ -2,13 +2,18 @@ import os
 import dotenv
 
 from langchain_redis import RedisConfig, RedisVectorStore
-# from langchain_huggingface import HuggingFaceEmbeddings
 
+#prompt template
 from langchain import hub
+
+# semantic cache
+from langchain.globals import set_llm_cache
+from langchain_community.cache import RedisSemanticCache
 
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.outputs import Generation
 
 import streamlit as st
 
@@ -20,7 +25,7 @@ os.environ["LANGSMITH_TRACING"] = dotenv.dotenv_values()["LANGSMITH_TRACING"]
 os.environ["LANGSMITH_PROJECT"] = dotenv.dotenv_values()["LANGSMITH_PROJECT"]
 
 REDIS_URL = dotenv.dotenv_values()["redis_vector_db"]
-EMBEDDING_MODEL = "sentence-transformers/paraphrase-MiniLM-L3-v2" # 17.4M params
+REDIS_SEMANTIC_CACHE_URL = dotenv.dotenv_values()["redis_semantic_cache"]
 PROMPT_TEMPLATE = "rlm/rag-prompt"
 LLM_MODEL = "llama3.2"
 
@@ -31,8 +36,10 @@ def format_docs(docs):
 
 @st.cache_resource
 def load_resources():
+    # query embedding model
     embedding_model = OllamaEmbeddings(model=LLM_MODEL)
-
+    
+    # vector db connection
     config = RedisConfig(
         index_name="article",
         redis_url=REDIS_URL
@@ -40,31 +47,47 @@ def load_resources():
 
     vector_store = RedisVectorStore(embeddings=embedding_model, config=config)
 
+    # generative LLM
     llm = ChatOllama(
         model=LLM_MODEL
     )
-
+    
+    # chat prompt template
     prompt = hub.pull(PROMPT_TEMPLATE)
 
+    # doc retrieval tool
     retriever = vector_store.as_retriever(
         search_type="similarity",
-        search_kwargs={"k": 6}
+        search_kwargs={"k": 4}
     )
 
+    # chain
     rag_chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
         | llm
         | StrOutputParser()
-
     )
 
     return rag_chain
 
 @st.cache_data
 def get_model_output(query):
-    response = rag_chain.invoke(query)
-    return response
+
+    # semantic cache check
+    result = cache.lookup(prompt=query, llm_string=LLM_MODEL)
+    print(f"\n\nRESULT: \n{result}\n")
+
+    if result:
+        print("Semantic cache hit:", result[0].text)
+        return result[0].text
+    
+    else:
+        print("Semantic cache miss")
+        response = rag_chain.invoke(query)
+        cache.update(prompt=query, llm_string=LLM_MODEL, return_val=[Generation(text=response)])
+        return response
+
 
 # streamlit app
 st.set_page_config(
@@ -75,6 +98,14 @@ st.set_page_config(
 # Initialize resources once
 rag_chain = load_resources()
 
+# semantic cache init
+cache = RedisSemanticCache(
+        redis_url=REDIS_URL,
+        embedding=OllamaEmbeddings(model=LLM_MODEL),
+        score_threshold=0.1
+        )
+
+set_llm_cache(value=cache)
 
 st.header("Know the How")
 form_input = st.text_input("Enter query")
